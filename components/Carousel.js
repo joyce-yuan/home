6346dccedee2f3f5e198622bc1de72367078ec04
Joyce-Yuan <jyuan24@mit.edu>
2023-09-04 12:23:52 -0700

    carousel

--- components/Carousel.js
+++ components/Carousel.js
@@ -79,6 +79,9 @@ class Carousel extends Component {
                       this.setState({ paused: false });
                     }}
                   />
+                  <div>
+                    <p>Hi testing</p>
+                  </div>
                 </div>
               );
             })}
